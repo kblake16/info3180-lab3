5eb1899314088d5715f40da8bfa5afc1e4973938
kblake16 <kayla.blake16@gmail.com>
2021-02-21 01:05:39 -0500

    Edit

--- app/views.py
+++ app/views.py
@@ -6,8 +6,10 @@ This file creates your application.
 """
 
 from app import app
+from app import forms
 from flask import render_template, request, redirect, url_for, flash
-
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -23,6 +25,20 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact/', methods =['POST','GET'])
+def contact():
+    form = forms.ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data, sender=(form.name.data,form.email.data),
+        recipients=["to@example.com"])
+        msg.body = form.message.data
+        mail.send(msg)
+        flash("Your message has been sent successfully")
+        return redirect(url_for('home'))
+    return render_template('contact.html',form=form)
+
+
 
 
 ###
